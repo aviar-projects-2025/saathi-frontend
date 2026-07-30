354e971f3c5c5aae8ebc58bdaf8347873fb42122
SakthivelAyyanar <sakthivel.a@aviartechservices.com>
2026-07-30 17:03:39 +0530

    Fixed

--- src/pages/RideDetails.jsx
+++ src/pages/RideDetails.jsx
@@ -647,7 +647,6 @@ export default function RideDetailsModal({
         />
       </Box>
 
-      {console.log("Data ==> ", ride)}
       <DialogContent sx={{ px: { xs: 2.5, sm: 4 }, py: { xs: 3, sm: 3.5 } }}>
         {/* ── Ticket data fields ── */}
         <Box
